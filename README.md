f91c1bcd56fc56a913128277083ee7d294cfbd9d
MaXiaojun <mxj@xiaojundeMacBook-Pro.local>
2020-09-01 18:13:19 +0800

    完成基础功能

--- README.md
+++ README.md
@@ -2,4 +2,77 @@
 
 ## 阿里云 Datahub Restful API 的 NodeJS 封装
 
-> 官网没有提供 NodeJS 版本的 SDK, 所以基于 Restful 接口文档自己封装了一下, 方便开发使用. 项目尚在开发中...
+> 官网没有提供 NodeJS 版本的 SDK, 所以基于 Restful 接口文档自己封装了一下, 方便开发使用. 
+
+**项目尚在开发中...**
+
+## Usage
+
+```sh
+yarn add aliyun-datahub-sdk
+```
+
+```js
+const Datahub = require("../lib/datahub");
+const { RecordType, FieldType, DatahubOptions, Field, 
+        RecordSchema, TupleRecord, BlobRecord, CursorType } = Datahub;
+
+
+// 创建 Datahub Client
+const datahubOption = new DatahubOptions(
+    "https://dh-cn-shanghai.aliyuncs.com",
+    process.env.ACCESS_KEY_ID,
+    process.env.ACCESS_KEY_SECRET
+);
+const dh = new Datahub(datahubOption);
+
+// 创建 Project
+dh.createProject('new_project', '测试创建 project')
+
+// 创建 Topic
+const testRecordSchema = new RecordSchema([
+    //[ name, type, notnull ]   
+    ['field_string', FieldType.STRING, true],
+    ['field_integer', FieldType.INTEGER, false],
+    ['field_boolean', FieldType.BOOLEAN, true],
+    ['field_timestamp', FieldType.TIMESTAMP, true]
+])
+dh.createTopic('new_project', 'new_tuple_topic', testRecordSchema, '测试创建 Tuple topic', RecordType.TUPLE))
+
+dh.createTopic('new_project', 'new_blob_topic', null, '测试创建 Blob topic', RecordType.BLOB))
+
+// 推送数据
+let testData = {
+    field_string: 'abc',
+    field_integer: 1,
+    field_boolean: false,
+    field_timestamp: new Date()
+}
+let testTupleRecords = []
+testTupleRecords.push(new TupleRecord(testData, testRecordSchema))
+dh.push('new_project', 'new_tuple_topic', testTupleRecords))
+
+let testBlobRecords = [new BlobRecord('test')]
+dh.push('new_project', 'new_blob_topic', testBlobRecords))
+
+// 拉取数据(从头开始)
+let _schema = (await dh.getTopic('new_project', 'new_tuple_topic')).data.RecordSchema
+let _cursor = (await dh.getCursor('new_project', 'new_tuple_topic')).data.Cursor
+let res = await dh.pull('new_project', 'new_tuple_topic', _schema, _cursor)
+_cursor = res.NextCursor
+let records = res.Records
+// ...更新 cursor 以循环获取数据
+
+// 如果是 blob 类型, schema 参数设置为空
+dh.pull('new_project', 'new_blob_topic', null, _firstCursor)
+
+
+
+```
+
+
+## Test
+
+```js
+yarn test
+```
--- test/datahub.spec.js
+++ test/datahub.spec.js
@@ -5,7 +5,7 @@ const Datahub = require("../lib/datahub");
 const assert = require('assert');
 
 
-const { RecordType, FieldType, Options, Field, RecordSchema, TupleRecord, BolbRecord } = Datahub
+const { RecordType, FieldType, DatahubOptions, Field, RecordSchema, TupleRecord, BlobRecord, CursorType } = Datahub
 
 // const dh = new Datahub({
 //     ENDPOINT: "https://dh-cn-shanghai.aliyuncs.com",
@@ -13,57 +13,12 @@ const { RecordType, FieldType, Options, Field, RecordSchema, TupleRecord, BolbRe
 //     ACCESS_KEY_SECRET: process.env.ACCESS_KEY_SECRET,
 // });
 
-const opt = new Options('https://dh-cn-shanghai.aliyuncs.com', process.env.ACCESS_KEY_ID, process.env.ACCESS_KEY_SECRET)
-const dh = new Datahub(opt)
+const datahubOption = new DatahubOptions('https://dh-cn-shanghai.aliyuncs.com', process.env.ACCESS_KEY_ID, process.env.ACCESS_KEY_SECRET)
+const dh = new Datahub(datahubOption)
 
-async function sleep(ms) {
-    console.log('sleep', ms);
-    return await new Promise(resolve => setTimeout(resolve, ms));
-}
 
-const spl = '\n---------------------------------\n\n'
-async function log(method, ...args) {
-    try {
-        console.log(method, await dh[method](...args), spl);
-    } catch (error) {
-        console.error(method, error, spl)
-    }
-}
-
-function test(expect, method, ...args) {
-    return async () => {
-        let res = await dh[method](...args);
-        assert.equal(expect, res.statusText);
-        console.log(JSON.stringify(res, null, 2));
-        sleep(1000);
-    }
-}
-
-async function prepare(method, ...args) {
-    try { 
-        await dh[method](...args);
-        sleep(1000);
-    } catch (error) { }
-
-}
-
-const testProjectName = 'jk_auto_test_project'
+const testProjectName = 'auto_test_project'
 const testTopicName = 'auto_test_topic'
-
-
-prepare('deleteProject', testProjectName)
-
-describe('Project 操作测试', function () {
-    it('创建', test('Created', 'createProject', testProjectName, 'test project'));
-    it('更新', test('OK', 'updateProject', testProjectName, '测试要删除的项目'));
-    it('查询', test('OK', 'getProject', testProjectName));
-    it('列表', test('OK', 'listProjects', testProjectName));
-    it('删除', test('OK', 'deleteProject', testProjectName));
-});
-
-
-prepare('createProject', testProjectName)
-
 const testRecordSchema = new RecordSchema([
     ['field_string', FieldType.STRING, true],
     ['field_integer', FieldType.INTEGER, true],
@@ -72,89 +27,169 @@ const testRecordSchema = new RecordSchema([
     ['field_decimal', FieldType.DECIMAL, true],
     ['field_timestamp', FieldType.TIMESTAMP, true],
 ])
-
-describe('Topic 操作测试', function() {
-    it('创建', test('Created', 'createTopic', testProjectName, testTopicName, testRecordSchema, 'test topic', RecordType.TUPLE));
-    sleep(1000)
-    it('更新', test('OK', 'updateTopic',testProjectName, testTopicName,'测试要删除的topic'));
-    it('查询', test('OK', 'getTopic', testProjectName, testTopicName));
-    it('列表', test('OK', 'listTopics', testProjectName, testTopicName));
-    it('删除', test('OK', 'deleteTopic', testProjectName, testTopicName));
-});
-
-
-async function test1() {
-    let res;
-    // console.log(dh);
-
-    // const dh2 = new Datahub({
-    //     ENDPOINT: "https://dh-cn-shanghai.aliyuncs.com",
-    //     ACCESS_KEY_ID: 'xxxx',
-    //     ACCESS_KEY_SECRET: 'xxxx' ,
-    // });
-
-    console.log('============= start ============>');
-
-
-    let recordSchema = new RecordSchema([
-        ['field_string', FieldType.STRING, true],
-        ['field_integer', FieldType.INTEGER, true],
-        ['field_double', FieldType.DOUBLE, true],
-        ['field_boolean', FieldType.BOOLEAN, true],
-        ['field_decimal', FieldType.DECIMAL, true],
-        ['field_timestamp', FieldType.TIMESTAMP, true],
-    ])
-
-    // console.log(recordSchema);
-
-    let data = {
-        field_string: 'abc',
-        field_integer: 1,
-        field_double: 12.1,
-        field_boolean: false,
-        field_timestamp: 'Sun Aug 30 2020 22:03:35 GMT+0800 (GMT+08:00)',
-        // field_decimal: 123.5
-    }
-    // let record = ['abc', '1', '12.1', 'false', String(Date.now()), '123.5']
-    // let record = ['abc', '1', '12.1', 'false', String(Date.now())]
-
-
-
-    // console.log(_record2Data(record, recordSchema));
-    // console.log(_data2Record(data, recordSchema))
-    // try {
-    //     res = await dh.createTopic('jk_test1','topic3', recordSchema, 'test topic3', RecordType.TUPLE)
-    // } catch (error) {
-    //     console.log('xx----->>', error);
-    // }
-
-    // dh.createTopic('jk_test1','topic6', recordSchema, 'test topic', RecordType.TUPLE)
-    // dh.createTopic('jk_test1','topic8', recordSchema)
-    // dh.deleteTopic('jk_test1','topic3')
-    // dh.updateTopic('jk_test1','topic2','update topic...')
-    // dh.getTopic('jk_test1','topic2')
-    // dh.getTopic('jk_test1','topic5')
-    // dh.listTopics('jk_test1')
-    // dh.getCursor('jk_test1','topic2', 0 , 'OLDEST')
-    // dh.getCursor('jk_test1','topic2')
-
-
-    // dh.pull('jk_test1', 'topic5', recordSchema, '30000000000000000000000000000000')
-    // .then(res => console.log(JSON.stringify(res),res.data.Records))
-    // .catch(err => console.error('xxxx',err))
-
-    // let records = []
-    // records.push(new TupleRecord(data, recordSchema))
-    // dh.push('jk_test1','topic5', records)
-    // // .then(res => console.log(JSON.stringify(res),res.data.RecordSchema))
-    // .then(res => console.log(JSON.stringify(res)))
-    // .catch(err => console.error('xxxx',err))
-
-    // dh.openOffsetSession()
-
-
-    console.log('\n<============= end =============');
-
+let testData = {
+    field_string: 'abc',
+    field_integer: 1,
+    field_double: 12.1,
+    field_boolean: false,
+    field_decimal: 123.5,
+    field_timestamp: 'Sun Aug 30 2020 22:03:35 GMT+0800 (GMT+08:00)'
 }
 
-// test()
+let testTupleRecords = []
+testTupleRecords.push(new TupleRecord(testData, testRecordSchema))
+
+
+
+describe('Modules 单元测试', function () {
+    describe('Record', function () {
+        it('new BlobRecord', () => {
+            assert.deepEqual({
+                Attributes: {foo:"bar"},
+                Data: 'dGVzdA==',
+                ShardId: '8'
+            }, new BlobRecord('test', {foo:"bar"}, '8'))
+        })
+
+        it('BlobRecord.base64Decode', () => {
+            let testStr = 'test'
+            let testBuffer = Buffer.from(testStr)
+            let blobRecord = new BlobRecord(testStr)
+            assert.deepEqual(testBuffer, BlobRecord.base64Decode(blobRecord.Data))
+        })
+    })
+})
+
+
+describe('Datahub 功能测试', function () {
+
+    describe('Project/Topic 操作测试', async function () {
+
+        // it('createProject', async () => {
+        //     assert.strictEqual('Created',
+        //         (await dh.createProject(testProjectName, '测试创建 project')).statusText
+        //     )
+        // })
+        // it('updateProject', async () => {
+        //     assert.strictEqual('OK',
+        //         (await dh.updateProject(testProjectName, '测试更新 project')).statusText
+        //     )
+        // })
+        // it('getProject', async () => {
+        //     assert.strictEqual('OK',
+        //         (await dh.getProject(testProjectName)).statusText
+        //     )
+        // })
+        // it('listProjects', async () => {
+        //     assert.strictEqual('OK',
+        //         (await dh.listProjects(testProjectName)).statusText
+        //     )
+        // })
+
+        // it('createTopic-tuple', async () => {
+        //     assert.strictEqual('Created',
+        //         (await dh.createTopic(testProjectName, testTopicName,  testRecordSchema, '测试创建 Tuple topic', RecordType.TUPLE)).statusText
+        //     )
+        // })
+
+        // it('createTopic-blob', async () => {
+        //     assert.strictEqual('Created',
+        //         (await dh.createTopic(testProjectName, testTopicName, null, '测试创建 Blob topic', RecordType.BLOB)).statusText
+        //     )
+        // })
+
+
+
+
+        // it('updateTopic', async () => {
+        //     assert.strictEqual('OK',
+        //         (await dh.updateTopic(testProjectName, testTopicName, '测试更新描述')).statusText
+        //     )
+        // })
+
+        // it('getTopic', async () => {
+        //     let res = await dh.getTopic(testProjectName, testTopicName)
+        //     assert.strictEqual('OK', res.statusText)
+        //     console.log(res.data);
+        // })
+
+        // it('push-tuple', async () => {
+        //     assert.strictEqual('OK',
+        //         (await dh.push(testProjectName, testTopicName, testRecords)).statusText
+        //     )
+        // })
+
+        // it('push-blob', async () => {
+        //     let _testBlobRecords = [new BlobRecord('test')]
+        //     assert.strictEqual('OK',
+        //         (await dh.push(testProjectName, testTopicName, _testBlobRecords)).statusText
+        //     )
+        // })
+        
+        // it('getCursor-default', async () => {
+        //     assert.strictEqual('OK',
+        //         (await dh.getCursor(testProjectName, testTopicName)).statusText
+        //     )
+        // })
+        // it('getCursor-oldest', async () => {
+        //     assert.strictEqual('OK',
+        //         (await dh.getCursor(testProjectName, testTopicName, CursorType.OLDEST)).statusText
+        //     )
+        // })
+        // it('getCursor-latest', async () => {
+        //     assert.strictEqual('OK',
+        //         (await dh.getCursor(testProjectName, testTopicName, CursorType.LATEST)).statusText
+        //     )
+        // })
+        // it('getCursor-sequence', async () => {
+        //     assert.strictEqual('OK',
+        //         (await dh.getCursor(testProjectName, testTopicName, CursorType.SEQUENCE, '0', 0)).statusText
+        //     )
+        // })
+        // it('getCursor-system_time', async () => {
+        //     let timestamp = new Date('2020-08-28').getTime()
+        //     assert.strictEqual('OK',
+        //         (await dh.getCursor(testProjectName, testTopicName, CursorType.SYSTEM_TIME, '0', timestamp)).statusText
+        //     )
+        // })
+
+ 
+        // it('pull-tuple', async () => {
+        //     let cursor  = (await dh.getCursor(testProjectName, testTopicName)).data.Cursor
+        //     let res = await dh.pull(testProjectName, testTopicName, testRecordSchema, cursor)
+        //     assert.strictEqual('OK', res.statusText);
+        //     console.log(res.data);
+        // })
+        it('pull-blob', async () => {
+            let cursor  = (await dh.getCursor(testProjectName, testTopicName)).data.Cursor
+            let res = await dh.pull(testProjectName, testTopicName, null, cursor)
+            assert.strictEqual('OK', res.statusText);
+            console.log(res.data);
+        })
+
+
+        // it('appendField', async () => {
+        //     assert.strictEqual('OK',
+        //         (await dh.appendField(testProjectName, testTopicName, 'field_append', FieldType.STRING)).statusText
+        //     )
+        // })
+        // it('listTopics', async () => {
+        //     assert.strictEqual('OK',
+        //         (await dh.listTopics(testProjectName, testTopicName)).statusText
+        //     )
+        // })
+        // it('deleteTopic', async () => {
+        //     assert.strictEqual('OK',
+        //         (await dh.deleteTopic(testProjectName, testTopicName)).statusText
+        //     )
+        // })
+
+
+        // it('deleteProject', async () => {
+        //     assert.strictEqual('OK',
+        //         (await dh.deleteProject(testProjectName)).statusText
+        //     )
+        // })
+    });
+
+})
